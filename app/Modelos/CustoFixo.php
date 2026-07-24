8bffa413b9859df89f1311f1fe1f98c086cfaf04
Wellington Xavier <wellington222688@gmail.com>
2026-07-24 11:44:21 -0400

    adiciona: app/Modelos/CustoFixo.php

---
+++ app/Modelos/CustoFixo.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aplicacao\Modelos;
+
+final class CustoFixo
+{
+    /** @param array<string, mixed> $dados */
+    public function __construct(private readonly array $dados)
+    {
+    }
+
+    public function obter(string $campo): mixed
+    {
+        return $this->dados[$campo] ?? null;
+    }
+}
